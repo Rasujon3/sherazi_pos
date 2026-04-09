b3111cc251ab5396103a9105bd5f86a480e5152c
Rasujon3 <rasujon3@gmail.com>
2026-04-10 01:37:45 +0600

    implement Redis cache & api resources on product list  & order list endpoints

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,14 +8,6 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return redirect()->route('user-index');
-    }
-
-    public function loginPage()
-    {
-        if (auth()->check()) {
-            return redirect()->route('dashboard');
-        }
-    	return view('admin_login');
+        return 'POS Application';
     }
 }
