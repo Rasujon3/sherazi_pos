e182985bcf7ca1549dc51d3fc860c9d01402b7ec
Rasujon3 <rasujon3@gmail.com>
2026-04-10 22:40:32 +0600

    rate limit add

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,19 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/search', [ProductController::class, 'search']);
-Route::get('/products/dashboard', [ProductController::class, 'dashboard']);
-Route::get('/products/sales-report', [ProductController::class, 'salesReport']);
+Route::middleware('throttle:100,1')->group(function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/search', [ProductController::class, 'search']);
+    Route::get('/products/dashboard', [ProductController::class, 'dashboard']);
+    Route::get('/products/sales-report', [ProductController::class, 'salesReport']);
 
-Route::get('/orders', [OrderController::class, 'index']);
-Route::get('/orders/filter', [OrderController::class, 'filterByStatus']);
+    Route::get('/orders', [OrderController::class, 'index']);
+    Route::get('/orders/filter', [OrderController::class, 'filterByStatus']);
+});
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('throttle:10,1')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
     Route::post('/orders', [OrderController::class, 'store']);
